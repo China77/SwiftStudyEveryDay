597a9f15f60bf5bae8a8df6590a47734ad531958
yuchuangu85 <yuchuangu85@gmail.com>
2018-08-26 11:14:44 +0800

    提交代码

---
+++ SwiftDemo/Swift02_BasicOperators.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  SwiftStudyDemo
+//
+//  Created by YUCHUAN on 2018/8/23.
+//  Copyright © 2018 YUCHUAN. All rights reserved.
+//
+
+import Foundation
---
+++ SwiftDemo/main.swift
@@ -0,0 +1,29 @@
+//
+//  main.swift
+//  SwiftDemo
+//
+//  Created by YUCHUAN on 16/06/2017.
+//  Copyright © 2017 YUCHUAN. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
+
+func start() -> Int {
+    let constant = 10
+    
+    print(constant)
+    return constant
+    
+}
+
+
+
+
+
+
+
+
+
